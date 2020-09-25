38324a0bdf2182c35aa3f160ca065936b21a9f48
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-25 21:25:05 +0330

    edit index view

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,6 +1,23 @@
-<div class="uk-container uk-container-small uk-text-center uk-margin-large-top">
+<div class="uk-container uk-padding uk-container-small uk-text-center uk-margin-large-top">
 
-  <h2>{{$index->name->data}}</h2>
+
+  <div class="uk-flex-bottom uk-flex-center " uk-grid>
+
+    <div class="uk-width-auto@m">
+      <img style="width:75px;" class="" src="@url('library/icon/svgs/solid/feather-alt.svg')" alt="">
+    </div>
+
+    <div class="uk-width-auto@m">
+      <h1>{{$index->name->data}}</h1>
+    </div>
+
+  </div>
+
+  <div class="uk-margin-top">
+    <h2 style="font-size:20px;" >{!! $index->whay->data !!}</h2>
+  </div>
+
+
 
   <div class="uk-padding">
 
@@ -9,7 +26,7 @@
     </div>
 
     <div class="uk-margin">
-      <button class="uk-button" style="background: #20773e;color: white;"> شروع به کار </button>
+      <a href="{{url('docs')}}" class="uk-button" style="background: #20773e;color: white;"> شروع به کار </a>
     </div>
 
   </div>
