3acaa0099f2a19e9be55fd71973cdefe0ba4558d
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-25 21:23:52 +0330

    edit header

--- app/views/site/header.php
+++ app/views/site/header.php
@@ -1,13 +1,13 @@
-<nav class="uk-navbar-container uk-navbar-transparent uk-padding-large uk-padding-remove-vertical" uk-navbar>
+<nav class="uk-navbar-container uk-navbar-transparent uk-padding-large uk-padding-remove-vertical" style="background: #1e87f0;" uk-navbar>
     <div class="uk-navbar-left">
 
         <ul class="uk-navbar-nav">
             @if($site->nav_brand??false)
-            <li class="uk-active"><a href="@url($site->nav_brand->value)">{{$site->nav_brand->data}}</a></li>
+            <li class="uk-active"><a class="uk-text-bold" style="color:white;" href="@url($site->nav_brand->value)">{{$site->nav_brand->data}}</a></li>
             @endif
 
             @foreach($site->nav as $key=>$nav)
-            <li><a href="{{url($nav->value)}}">{{$nav->data}}</a></li>
+            <li><a style="color:white;" href="{{url($nav->value)}}">{{$nav->data}}</a></li>
             @endforeach
         </ul>
 
